e22dba366cce9b51a6b9ed21a5258c4267380800
mann909 <mann.soni909@gmail.com>
2025-11-13 13:04:35 +0530

    refactored

---
+++ src/AppInitializer.tsx
@@ -0,0 +1,84 @@
+// import { useQuery } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+// import { useEffect, useState } from "react";
+// import AppLoader from "./components/empty-states/AppLoader";
+import AppShimmer from "./components/empty-states/AppShimmer";
+import GlobalNotFound from "./components/empty-states/GlobalNotFound";
+// import { routeTree } from "./routeTree.gen";
+// import { useAuthStore } from "./store/useAuthStore";
+// import { TokenUtil } from "./utils/tokenUtil";
+import { queryClient } from "./lib/queryClient";
+
+export const router = createRouter({
+  routeTree,
+  context: {
+    isLoggedIn: false,
+    queryClient,
+  },
+  scrollRestoration: true,
+  defaultErrorComponent: (e : any) => <GlobalNotFound error={e?.error} />,
+  defaultPendingComponent: () => <AppShimmer />,
+  defaultNotFoundComponent: () => <GlobalNotFound />,
+});
+
+export type AppRouter = typeof router;
+
+const AuthInitializer = () => {
+  // const token = TokenUtil.getToken();
+  // const { isLoggedIn, loginFail, setUser } = useAuthStore();
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // const { data: permissions, isLoading: isLoadingPermissions } = useQuery({
+  //   ...authQueries.getPermissions(),
+  //   enabled: !!token,
+  // });
+
+  // const checkIsAuthenticated = async () => {
+  //   try {
+  //     const user = await authService.getProfile();
+  //     if (user?.uniqueId) {
+  //       setUser(user);
+  //     } else {
+  //       loginFail();
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     loginFail();
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+
+  // useEffect(() => {
+  //   if (!token) {
+  //     loginFail();
+  //     setIsLoading(false);
+  //     return;
+  //   }
+  //   checkIsAuthenticated();
+  // }, []);
+
+  return (
+    <>
+      {/* <AppLoader isLoading={isLoading} /> */}
+      {!false && ( // change to isLoggedIn when auth is enabled 
+        <RouterProvider
+          router={router}
+          context={{
+            isLoggedIn: false, // change to isLoggedIn when auth is enabled 
+            queryClient,
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default AuthInitializer;
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as Sentry from "@sentry/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import AuthInitializer from "./AppInitializer.tsx";
+import { queryClient } from "./lib/queryClient.ts";
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement, {
+    onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+      console.warn("Uncaught error", error, errorInfo.componentStack);
+    }),
+    onCaughtError: Sentry.reactErrorHandler(),
+    onRecoverableError: Sentry.reactErrorHandler(),
+  });
+
+  
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <AuthInitializer />
+    </QueryClientProvider>,
+  );
+}
